8954d72bc0eec9cfc4a53a163debeaaa495bad74
Oswald Calvario <oswald.calvario@gmail.com>
2020-07-10 22:39:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // Code your solutions in this file
-function printBadges(names) {
-  for (let count = 0; count < names.length; count++) {
-    console.log(`Welcome ${names[count]}! You are employee #${count + 1}.`)
-  }
-  return names
+function writeCards(names, event) {
+    let cards = []; 
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return cards
 }
 
 function countdown(integer) {
