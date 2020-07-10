c0f9c158e3edc75cff2aa94d44affc833b050476
Oswald Calvario <oswald.calvario@gmail.com>
2020-07-10 22:40:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let cards = []; 
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return cards
+}
+
+function countDown(startNumber) {
+  let currentNumber = startNumber
+  while (currentNumber > -1){
+    console.log(currentNumber)
+    currentNumber -= 1
+  }
+}
