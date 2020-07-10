3b086f6408beaa0e3afa982d580b82177f90cf5d
Oswald Calvario <oswald.calvario@gmail.com>
2020-07-10 22:33:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function printBadges(names) {
+  for (let count = 0; count < names.length; count++) {
+    console.log(`Welcome ${names[count]}! You are employee #${count + 1}.`)
+  }
+  return names
+}
+
+function tailsNeverFails() {
+  counter = 0
+  coin = Math.random()
+
+  while(coin >= 0.5) {
+    coin = Math.random()
+    counter++
+  };
+
+  return `You got ${counter} tails in a row!`
+}
